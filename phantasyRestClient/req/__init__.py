aac07fa3e40b910465d5d0cb9f3ec9186d382aab
Tong Zhang <zhangt@frib.msu.edu>
2022-04-07 10:12:02 -0400

    REF: Do not verify SSL, workaround for SSL error.

--- phantasyRestClient/req/__init__.py
+++ phantasyRestClient/req/__init__.py
@@ -3,6 +3,7 @@ from phantasyRestClient.config import conf_dict
 from phantasyRestClient.req.mp import MachinePortalResources
 
 session = requests.Session()
+session.verify = False
 # mp resources
 MachinePortalResources.SESSION = session
 MachinePortalResources.URL = conf_dict['bind']
