273037b7f612dfa53183083ecb3a1e0230c89edd
Tong Zhang <zhangt@frib.msu.edu>
2022-04-07 10:49:27 -0400

    MNT: Bump version to 0.1.5.

--- phantasyRestClient/__init__.py
+++ phantasyRestClient/__init__.py
@@ -1,7 +1,7 @@
 from .req import MachinePortalResources
 
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 __author__ = 'Tong Zhang <zhangt@frib.msu.edu>'
 __doc__ ="""phantasyRestClient: Python interface to PHANTASY REST API."""
 
