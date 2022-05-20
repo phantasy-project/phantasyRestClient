b8af4193b095a18352ffe7b299a2862c991ead5c
Tong Zhang <zhangt@frib.msu.edu>
2022-05-20 10:38:27 -0400

    TST: Add tests for calcEnergyLoss APIs.

--- phantasyRestClient/req/mp.py
+++ phantasyRestClient/req/mp.py
@@ -136,16 +136,28 @@ class MachinePortalResources:
         return r.json()
 
     @staticmethod
-    def calcEnergyLoss(projectile: dict, material: dict) -> dict:
-        """Calculate energy loss with ATIMA.
+    def calcEnergyLoss(projectile: dict, material: dict):
+        """Calculate energy loss with ATIMA, returns a dict.
         """
         r = MachinePortalResources.SESSION.post(
                 f"{MachinePortalResources.URL}/physics/atima/calc",
                 data=json.dumps({
-                    'projectile': projectile,
-                    'material': material,
+                    "projectile": projectile,
+                    "material": material,
                 }))
         if not r.ok:
             return None
         return r.json()
 
+    @staticmethod
+    def calcEnergyLossList(projectile: dict, material: dict):
+        """Calculate energy loss with ATIMA, returns a list.
+        """
+        r = MachinePortalResources.calcEnergyLoss(projectile, material)
+        if r is not None:
+            return [r[k] for k in ('Ek', 'dE/dx', 'Ek_std',
+                                   'range_in', 'range_out',
+                                   'range_std', 'angle_std',
+                                   'q_in', 'q_out')]
+        else:
+            return []
