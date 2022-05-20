16d65b9f6938dde5dd234624cbfc113679025605
Tong Zhang <zhangt@frib.msu.edu>
2022-05-20 11:36:55 -0400

    REF: caget support passing one PV, or a list of PVs.

--- phantasyRestClient/req/ca.py
+++ phantasyRestClient/req/ca.py
@@ -3,6 +3,7 @@
 """Request for resources from ca (Channel Access) router.
 """
 import json
+from typing import List, Union
 
 
 class CAResources:
@@ -13,15 +14,14 @@ class CAResources:
         pass
 
     @staticmethod
-    def caget(pvname: str, datatype: str):
-        """Return the value of *pvname*.
+    def caget(pvname: Union[List[str], str], datatype: str = None):
+        """Return the values of a list of PVs, if only one string of
+        PV is passed, return a single value.
         """
-        r = CAResources.SESSION.get(
+        r = CAResources.SESSION.post(
             f"{CAResources.URL}/epics/caget",
-            params={
-                'pvname': pvname,
-                'datatype': datatype
-                })
+            data=json.dumps(pvname),
+            params={'datatype': datatype})
         if not r.ok:
             return None
         return r.json()
